6dfec95f360296dd61d516baeb6a66705e9a837f
dimpurr <dimpurr@live.com>
2019-03-12 17:25:08 +0800

    add session

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var everyauth = require('everyauth');
+var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +20,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(express.bodyParser());
+// app.use(express.session());
+app.use(session({
+  secret: 'dimagine',
+  cookie: {
+    maxAge: 60000
+  },
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(everyauth.middleware(app));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dimagine' });
+  if (req.session.views) {
+    req.session.views++
+  } else {
+    req.session.views = 1
+  }
+  res.render('index', {
+    title: 'Dimagine',
+    session_views: req.session.views
+  });
 });
 
 module.exports = router;
